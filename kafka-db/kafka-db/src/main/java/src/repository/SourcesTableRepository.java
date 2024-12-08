e1b408b3149be3db8703139d5c039d8b5c6b4e9b
Polina Chirikova <polia.chirikova@ya.ru>
2024-12-08 20:51:30 +0400

    add debezium with need source data

--- kafka-db/kafka-db/src/main/java/src/repository/SourcesTableRepository.java
+++
@@ -1,7 +0,0 @@
-package src.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import src.models.SourcesTable;
-
-public interface SourcesTableRepository extends JpaRepository<SourcesTable, Integer> {
-}
